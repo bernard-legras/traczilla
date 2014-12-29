228b7138db1d5e12bc406a3c2ad2e1cf2122227b
Bernard Legras <legras@lmd.ens.fr>
2014-12-29 13:03:49 +0100

    Do not compile the tests controlling the calculation of theta in the interpolation code when the code is in parallel mode.

--- isentrop_m.f90
+++ isentrop_m.f90
@@ -107,6 +107,9 @@ subroutine interpol_wind_theta   &
 ! Calculates the theta values on the four adjacent columns if required
 !*********************************************************************
 
+#if defined(PAR_RUN)
+
+#else
       if(.not.theta_col(ix,jy,memind(1)))   call calc_col_theta(ix,jy,memind(1))
       if(.not.theta_col(ix,jy,memind(2)))   call calc_col_theta(ix,jy,memind(2))
       if(.not.theta_col(ix,jyp,memind(1)))  call calc_col_theta(ix,jyp,memind(1))
@@ -115,6 +118,7 @@ subroutine interpol_wind_theta   &
       if(.not.theta_col(ixp,jy,memind(2)))  call calc_col_theta(ixp,jy,memind(2))
       if(.not.theta_col(ixp,jyp,memind(1))) call calc_col_theta(ixp,jyp,memind(1))
       if(.not.theta_col(ixp,jyp,memind(2))) call calc_col_theta(ixp,jyp,memind(2))
+#endif
 
 
 ! Determine the level below the current position for u,v
